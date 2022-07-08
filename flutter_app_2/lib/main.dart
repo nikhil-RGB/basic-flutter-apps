17d5c47be7d63bdf645e37b54b74e17571629ab2
nikhil-RGB <bleach.ichigo.anime@gmail.com>
2022-07-08 13:01:44 +0530

    Create hello-title-display app

---
+++ flutter_app_2/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new MaterialApp(home: Text('Hello'));
+  }
+}
